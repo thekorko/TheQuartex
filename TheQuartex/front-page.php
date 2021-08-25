091cd5558bdcfc81fd1599e726d3c552e8348686
Sebas Korko <frozedviking@gmail.com>
2021-08-24 22:32:57 -0300

    update

--- TheQuartex/front-page.php
+++ TheQuartex/front-page.php
@@ -42,24 +42,30 @@ get_header();
 					'order'   => 'ASC'
 				) );
 				$cats_query = array( 'memes','random_es','random_en','memes_es' );
+				$counter = 0;
 				foreach ($categories as $category) {
 					//TODO fix it doesnt work
 					if (in_array(strtolower($category->name),$cats_query)) {
 						//it never is true
-						array_push($excludedcats, $category->term_id);
+						$excludedcats[$counter] = $category->term_id;
+						//array_push($excludedcats, $category->term_id);
+						//var_dump($category->term_id);
+						$counter++;
 					}
-					//var_dump($excludedcats);
 				}
+				//print_r($excludedcats);
 
 				$post_query_args = array(
 						//we only get roles_query userIDs see above get_users() query
 				    'post_type' => 'post',
 						'posts_per_page' => 15,
+						'post__not_in' => get_option( 'sticky_posts' ),
+						'ignore_sticky_posts' => 1,
 						'post_status' => $statuses,
 						//get paged, this is a secured wordpress way of getting pages
 						'paged' => $paged,
 						//tax query to find only posts from the cats_query categories taxonomy
-						'category__not_in', $excludedcats
+						'category__not_in' => $excludedcats
 				);
 				$query = new WP_Query($post_query_args); ?>
 			  <?php while ( $query->have_posts() ) : $query->the_post(); ?>
@@ -85,7 +91,7 @@ get_header();
 						}
 						?>
 
-					<?php if (($j % 5) == 0) :
+					<?php if (($j % 6) == 0) :
 						$j = $j + 1; //hack horrible y asqueroso arreglar
 						//si ponia este if abajo del otro if
 					?>
