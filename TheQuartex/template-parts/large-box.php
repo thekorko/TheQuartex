73d521f54d5c82061a1b32ba6fb04441ab6c03c3
Sebas Korko <frozedviking@gmail.com>
2022-01-16 18:44:26 -0300

    ads in large box

--- TheQuartex/template-parts/large-box.php
+++ TheQuartex/template-parts/large-box.php
@@ -11,6 +11,17 @@
 ?>
 
 <div id="large-box" class="large-box base-box">
-  <!--large-box-->
 
+  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5750670113076133"
+       crossorigin="anonymous"></script>
+  <!-- large_box -->
+  <ins class="adsbygoogle"
+       style="display:block"
+       data-ad-client="ca-pub-5750670113076133"
+       data-ad-slot="4612452872"
+       data-ad-format="auto"
+       data-full-width-responsive="true"></ins>
+  <script>
+       (adsbygoogle = window.adsbygoogle || []).push({});
+  </script>
 </div>
