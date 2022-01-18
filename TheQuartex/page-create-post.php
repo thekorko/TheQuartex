360185fc6a1c36aaab3be2628cad78046263a01d
Sebas Korko <frozedviking@gmail.com>
2022-01-18 17:48:03 -0300

    implementar editor en frontend bien hecho

--- TheQuartex/page-create-post.php
+++ TheQuartex/page-create-post.php
@@ -17,6 +17,9 @@ Template Name: create-post
  */
 
  get_header();
+ $post_type = '';
+ $message = '';
+ $post_value = '';
  ?>
  <style>
  #create-post-page {
@@ -42,10 +45,6 @@ Template Name: create-post
  	<?php get_sidebar(); ?>
  	<main id="main" class="main-content">
  	<div id="create-post-page" class="base-box singlepost-box">
-
-      <?php
-      while ( have_posts() ) :
-        the_post(); ?>
         <article id="create-post" <?php post_class(); ?>>
           <header class="entry-header">
             <h1 class="entry-title">
@@ -55,20 +54,87 @@ Template Name: create-post
           <div class="entry-content">
             <?php
             if (is_user_logged_in()) {
+              if (isset($_GET['post_type'])) {
+                $post_type = post_type($_GET['post_type']);
+                if (isset($_GET['message'])) {
+                  $message = message($_GET['message']);
+                }
+              }
               $user = wp_get_current_user();
               $publish_roles = array('editor', 'administrator', 'author', 'qtx_user', 'qtx_mod', 'qtx_vip');
               $review_roles = array('qtx_noob', 'subscriber', );
-              if ((array_intersect($publish_roles, $user->roles))) {
-                //post form
-                echo do_shortcode( "[wpuf_form id='629']" );
-              } elseif ((array_intersect($review_roles, $user->roles))) {
-                //noob post form
-                echo do_shortcode( "[wpuf_form id='880']" );
+
+              //Old post fomr from wpuf
+              if (empty($post_type)) {
+                if ((array_intersect($publish_roles, $user->roles))) {
+                  //post form
+                  echo do_shortcode( "[wpuf_form id='629']" );
+                } elseif ((array_intersect($review_roles, $user->roles))) {
+                  //noob post form
+                  echo do_shortcode( "[wpuf_form id='880']" );
+                } else {
+                  //noob
+                  echo do_shortcode( "[wpuf_form id='880']" );
+                }
+
               } else {
-                //noob
-                echo do_shortcode( "[wpuf_form id='880']" );
+                //Switcdh case of form type, form type is based on the post_type we are dealing with
+                $editor_class = "wp-core-ui wp-editor-wrap tmce-active";
+                $editor_name = "post_form";
+                switch ($post_type):
+                    case 'random':
+                        $editor_settings = [
+                            'textarea_rows' => 10,
+                            'quicktags'     => false,
+                            'media_buttons' => false,
+                            'plugins' => 'tabfocus,image,editimage,paste,media,fullscreen,wordpress,wpeditimage,wpgallery,wplink,wpdialogs,emoticons',
+                            'tinymce'       => array(
+                                'toolbar1'      => 'blockquote,image,media,link,unlink,italic,bold,underline,alignleft,aligncenter,alignright,undo,redo,charmap,emoticons',
+                                'toolbar2'      => '',
+                                'toolbar3'      => '',
+                              ),
+                            'teeny'         => false,
+                            'editor_class'  => $editor_class,
+                            'textarea_name' => $editor_name,
+                        ];
+                        break;
+                    case 1:
+                        echo "i equals 1";
+                        break;
+                    case 2:
+                        echo "i equals 2";
+                        break;
+                    default:
+                    $editor_settings = [
+                        'textarea_rows' => 10,
+                        'quicktags'     => false,
+                        'media_buttons' => true,
+                        'teeny'         => true,
+                        'editor_class'  => $editor_class,
+                        'textarea_name' => $editor_name,
+                    ];
+                endswitch;
+                $share_imgur = get_template_directory_uri().'/img/share/imgur.png';
+                $img_share_imgur = '<a href="https://imgur.com/upload"><img src="'.$share_imgur.'"></img></a>';
+                $textarea_id = 'random_post';
+                $input_post_title = '<input class="textfield" id="post_title" type="text" name="post_title" placeholder="Ingresa el título del post" value="" size="40">';
+                // Turn on the output buffer
+                ob_start();
+
+                // Echo the editor to the buffer
+                wp_editor( $post_value, $textarea_id, $editor_settings );
+
+                // Store the contents of the buffer in a variable
+                $editor_contents = ob_get_clean();
+
+                // Return the content you want to the calling function
+                //return $editor_contents;
+                $input_post_tags = '<input class="textfield" id="post_tags" type="text" name="post_tags" placeholder="" value="" size="40" autocomplete="off">';
+                $input_post_submit = '<input class="textfield" id="post_publish" type="submit" name="post_publish" value="Submit" size="10">';
+
+                echo "$img_share_imgur<br>$input_post_title<br>$editor_contents<br>$input_post_tags<br>$input_post_submit";
               }
-              // code...
+
             } else {
               echo "<p>";
               qtx_string_e('not_logged_in');
@@ -82,10 +148,7 @@ Template Name: create-post
             ) );
             ?>
           </div>
-          <article>
-      <?php
-      endwhile; // End of the loop.
-      ?>
+        </article>
     </div>
  		</main>
  </div>
